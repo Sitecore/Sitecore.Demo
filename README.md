60748580375ab229b3e30105177f96dc276e8741
Thomas Eldblom <the@sitecore.net>
2016-04-12 18:15:58 +1000

    Synced the gulp tasks with Habitat Added README.md Removed unused Theme project Removed Experience Panel buttons from layout (now part of the demo module)

---
+++ README.md
@@ -0,0 +1,10 @@
+#Sitecore.Demo
+
+This solution hosts the demo websites for Sitecore.
+
+* Legal - *Professional services vertical. Legal company*
+
+To install, set up a website running the Habitat framework.  
+For more information about the Habitat project, please check out the [Habitat Wiki](https://github.com/sitecore/habitat/wiki)
+
+Please contact [Mike Carter (mca)](mca@sitecore.net) for more information about the demo projects.
--- gulp-config.js
+++ gulp-config.js
@@ -1,9 +1,10 @@
-module.exports = function () {
-    var config = {
-        websiteRoot: "C:\\websites\\Habitat.local\\Website",
-        sitecoreLibraries: "C:\\websites\\Habitat.local\\Website\\bin",
-        solutionName: "Sitecore.Demo",
-        buildConfiguration: "Debug"
-    }
-    return config;
+module.exports = function() {
+  var config = {
+    websiteRoot: "C:\\websites\\Habitat.local\\Website",
+    sitecoreLibraries: "C:\\websites\\Habitat.local\\Website\\bin",
+    solutionName: "Sitecore.Demo",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  };
+  return config;
 }
