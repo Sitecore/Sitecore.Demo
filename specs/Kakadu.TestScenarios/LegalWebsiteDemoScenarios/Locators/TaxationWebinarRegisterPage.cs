f18c3491c06cd6a56f418e8dee2b5e722405e2a5
Konstantin Teltov <dneprokos@gmail.com>
2016-04-25 13:24:09 +0300

    Files were comitted.

---
+++ specs/Kakadu.TestScenarios/LegalWebsiteDemoScenarios/Locators/TaxationWebinarRegisterPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary1.Locators
+{
+  public class TaxationWebinarRegisterPage
+  {
+    public static IWebDriver Driver => FeatureContext.Current.Get<IWebDriver>();
+
+    public static IEnumerable<IWebElement> FormFieldNames
+      =>
+        Driver.FindElements(By.CssSelector(".control-label"));
+  }
+}
