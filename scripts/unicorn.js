080b029965dcb63276a2dc68d362f389823284b3
Thomas Eldblom <the@sitecore.net>
2016-06-30 15:46:00 +1000

    Aligned with Habitat gulp scripts Fixed site order

--- scripts/unicorn.js
+++ scripts/unicorn.js
@@ -5,6 +5,10 @@ var glob = require("glob");
 var async = require("async");
 var fs = require("fs");
 
+var unquote = function(str){
+  return str.replace(/^"(.*)"$/,"$1");
+}
+
 module.exports = function (callback, options) {
   var getUnicornSecret = function () {
     if (options.secret)
@@ -34,3 +38,28 @@ module.exports = function (callback, options) {
     callback();
   });
 };
+
+module.exports.getFullItemPath = function (itemFile) {
+  var fileContent = itemFile.contents.toString();
+  var path = unquote(fileContent.match(/Path:\s*(.*)$/m)[1]);
+  var id = unquote(fileContent.match(/ID:\s*(.*)$/m)[1]);
+  var dbMatch = fileContent.match(/DB:\s*(.*)$/m);
+  if (dbMatch) {
+    var db = dbMatch[1];
+  } else {
+    var db = "master";
+  }
+  return "/" + db + path + "/{" + id + "}/invariant/0"
+}
+
+module.exports.getUserPath = function (userFile) {
+   var fileContent = userFile.contents.toString();
+   var userName = fileContent.match(/username:\s*(.*)$/m)[1];
+   return "users:"+userName;
+}
+
+module.exports.getRolePath = function (roleFile) {
+   var fileContent = roleFile.contents.toString();
+   var roleName = fileContent.match(/name:\s*(.*)$/m)[1];
+   return "roles:"+roleName;
+}
