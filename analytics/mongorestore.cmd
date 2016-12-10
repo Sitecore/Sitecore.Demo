0835a35c5eed677506821d613fb65b4a4607d0da
Thomas Eldblom <the@sitecore.net>
2016-12-10 18:02:56 +1100

    First iteration of Utilities content

---
+++ analytics/mongorestore.cmd
@@ -0,0 +1,4 @@
+"C:\MongoDB\bin\mongorestore.exe" --db "habitat_dev_local_analytics" ".\MongoData\analytics"
+"C:\MongoDB\bin\mongorestore.exe" --db "habitat_dev_local_tracking_contact" ".\MongoData\tracking.contact"
+"C:\MongoDB\bin\mongorestore.exe" --db "habitat_dev_local_tracking_history" ".\MongoData\tracking.history"
+"C:\MongoDB\bin\mongorestore.exe" --db "habitat_dev_local_tracking_live" ".\MongoData\tracking.live"
