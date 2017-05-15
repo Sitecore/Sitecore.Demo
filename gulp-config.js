5127e784a4bbbc7798717d9fe00c63c026915236
James Hirka <mail@jameshirka.com>
2017-05-15 16:03:39 +1000

    updated demo gulp script to be inline with habitat

--- gulp-config.js
+++ gulp-config.js
@@ -6,6 +6,9 @@ module.exports = function() {
     licensePath: instanceRoot + "\\Data\\license.xml",
     solutionName: "Sitecore.Demo",
     buildConfiguration: "Debug",
+    buildPlatform: "Any CPU",
+    buildToolsVersion: 14.0, //change to 15.0 for VS2017 support
+    publishPlatform: "AnyCpu",
     runCleanBuilds: false
   };
   return config;
